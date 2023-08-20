07dd44049719d79c8f2f4287478f1a341c9ebaf3
AdityaKirad <kiradaditya2001@gmail.com>
2023-08-20 17:30:34 +0530

    Initial Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  experimental: {
+    assets: true,
+  },
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    screens: {
+      sm: "40em",
+      md: "48em",
+      lg: "64em",
+      xl: "80em",
+      "2xl": "96em",
+    },
+    extend: {
+      height: {
+        dynamicScreen: "calc(var(--vh, 1vh) * 100)",
+      },
+    },
+  },
+  plugins: [],
+}
