d4cd284267188328fb5848df9cd42d72bd17612a
AdityaKirad <kiradaditya2001@gmail.com>
2023-08-20 17:44:42 +0530

    setuped sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://buy1gram-retailer.pages.dev",
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
   experimental: {
     assets: true,
   },
-});
+})
